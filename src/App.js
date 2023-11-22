38743a784be49ade364a3b501f4c46e703dc933d
justin-ngatho <93423017+justin-ngatho@users.noreply.github.com>
2023-11-22 06:40:25 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ControlledCarousel from './components/hero';
+
+function App() {
+  return (
+    <div className="App">
+      <ControlledCarousel/>
+    </div>
+  );
+}
+
+export default App;
